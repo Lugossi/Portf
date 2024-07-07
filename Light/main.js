d99926f44a7b33175d778ea2fbe8d4351b4492e9
Lugossi <nikolozberdzenishvili1@gmail.com>
2024-07-07 21:18:10 +0400

    A simple Portfolio

---
+++ Light/main.js
@@ -0,0 +1,9 @@
+// response-menu
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// responsive-menu-end
---
+++ main.js
@@ -0,0 +1,9 @@
+// responsive-menu
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// responsive-menu-end
